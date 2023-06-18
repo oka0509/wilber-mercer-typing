66d7cdefcb01802a2b5f708690aaace9cbe727ed
oka0509 <76876781+oka0509@users.noreply.github.com>
2023-06-18 21:35:34 +0900

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,18 @@ import { FC } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Game from "./components/Game";
+import Home from "./components/Home";
 
 const App: FC = () => {
   return (
     <>
       <h1>Typing game</h1>
-      <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="1" element={<Game level={1} />} />
           <Route path="2" element={<Game level={2} />} />
           <Route path="3" element={<Game level={3} />} />
         </Routes>
-      </BrowserRouter>
     </>
   );
 };
---
+++ src/components/Home.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+const Home: FC = () => {
+  return (
+    <ul>
+      <li><a href="/1">level1</a></li>
+      <li><a href="/2">level2</a></li>
+      <li><a href="/3">level3</a></li>
+    </ul>
+  )
+};
+
+export default Home;
